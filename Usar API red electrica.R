2ebb8cfab212720fae292976ed0791ada80ce6ee
Guillermo Follana-Berná <g.follana@gmail.com>
2022-09-14 12:37:49 +0200

    Actualizacion
    Añadida envio foto cam casa

--- Usar API red electrica.R
+++ Usar API red electrica.R
@@ -8,18 +8,23 @@ library(telegram.bot)
 # Initialize bot
 bot <- Bot(token = bot_token("GuilleFBbot"))
 
-# Get updates
 updates <- bot$getUpdates()
-
+# Get updates
+while(length(updates)==0){
+  updates <- bot$getUpdates()
+}
+  
+#Sys.sleep(2)
 # Retrieve your chat id
 # Note: you should text the bot before calling `getUpdates`
 
-if (length(updates)==0){
-  chat_id=1499074368 # numero sin comillas
-} else {
-  chat_id = updates[[1L]]$from_chat_id()  
-}
+# if (length(updates)==0){
+#   chat_id=1499074368 # numero sin comillas
+# } else {
+#   chat_id = updates[[1L]]$from_chat_id()  
+# }
 
+chat_id = updates[[1L]]$from_chat_id() 
 # Funciones del bot a preguntas del chat
 mañana <- function(bot, update) {
   fecha=Sys.Date()+1
@@ -130,12 +135,12 @@ preciosmañana <- function(bot, update) {
              ,"T24:00&time_trunc=hour"
              #&geo_ids=8743" #geo_ids de baleares
              ,sep="")
-
+  
   json_file=jsonlite::fromJSON(temp)
-
+  
   data=as.data.frame(json_file$included$attributes$values[[1]])
   data$value=data$value/1000
-
+  
   data$datetime=as.POSIXct(data$datetime, format = "%Y-%m-%dT%H:%M:%S")
   #
   # ggplot(data = data, aes(x = datetime, y = value))+
@@ -143,26 +148,26 @@ preciosmañana <- function(bot, update) {
   #   geom_point(color = "#00AFBB", size = 2)
   #
   # plot(data$datetime,data$value,ylim=c(0,500))
-
+  
   # data$datetime[which(data$value==min(data$value))]
   # data$datetime[which(data$value==max(data$value))]
-
+  
   # Las horas mas baratas
   Baratas=data[order(data$value)[1:3],c(1,3)]
   a=as.POSIXlt(Baratas$datetime)
-
+  
   # Las horas mas caras
   Caras=data[order(data$value, decreasing = T)[1:3],c(1,3)]
   b=as.POSIXlt(Caras$datetime)
-
+  
   # texto=paste("Mañana ", "*",as.character(fecha, format="%A, %d de %B de %Y"), "*",
   #             ", las horas mas baratas son: ", "*", paste(a$hour, collapse = ", "),"*",
   #             " y las horas mas caras son: ", "*",paste(b$hour, collapse = ", "), "*",
   #             sep = "")
-
+  
   texto=paste("*Precios por hora para el:*",as.character(fecha, format="%A, %d de %B de %Y"),
               paste(paste(sprintf("%.3f",round(data[,1],4)),as.data.frame(strsplit(as.character(data[,3]), " "))[2,],sep="  "),collapse = "\n"), sep="\n")
-
+  
   bot$sendMessage(
     chat_id = update$message$chat$id,
     text = texto, parse_mode = "Markdown"
@@ -223,11 +228,31 @@ precioshoy <- function(bot, update) {
 
 ####################
 
+# Send photo
+fotocasa <- function(bot, update) {
+  url.foto = paste("http://admin:admin@192.168.1.31:88/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=",
+                   unlist(strsplit(Sys.getenv("UserPassCamCasa"),":"))[1],
+                   "&pwd=",
+                   unlist(strsplit(Sys.getenv("UserPassCamCasa"),":"))[2],
+                   sep="")
+  Sys.sleep(2)
+  foto.name=paste(gsub(x = as.character(Sys.time()), pattern = ":", replacement = "_"), '.jpg', sep="")
+  invisible()
+  download.file(url.foto, foto.name, mode = 'wb', quiet = TRUE)
+  Sys.sleep(2)
+  bot$sendDocument(chat_id,
+                   document = paste(getwd(),"/",foto.name,sep="")
+  )
+}
+
+####################
+
 updater <- Updater(bot_token("GuilleFBbot")) +
   CommandHandler("mañana", mañana) +
   CommandHandler("hoy", hoy) +
   CommandHandler("preciosmañana", preciosmañana) +
   CommandHandler("precioshoy", precioshoy) +
+  CommandHandler("fotocasa", fotocasa) +
   CommandHandler("start", start)
 
 updater$start_polling()
